f2abf58e258bbca68350dc5aae97ffaac56a911d
envoy-docs(travis) <envoy-docs@users.noreply.github.com>
2018-09-14 22:28:54 +0000

    docs envoy@35ec541eca7fcd0c7d890fedb4731df35512e498

--- docs/envoy/latest/_static/documentation_options.js
+++ docs/envoy/latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: '',
-    VERSION: '1.8.0-dev-e29d23',
+    VERSION: '1.8.0-dev-35ec54',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
