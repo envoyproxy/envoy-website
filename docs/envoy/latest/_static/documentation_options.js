30eac9a8269db01a6a3e10e9f0e8b412b8dceb6e
envoy-docs(travis) <envoy-docs@users.noreply.github.com>
2019-06-08 19:50:11 +0000

    docs envoy@2a59bb3d7732228ef5ec71b8c51f626b84be3455

--- docs/envoy/latest/_static/documentation_options.js
+++ docs/envoy/latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.11.0-dev-ea7243',
+    VERSION: '1.11.0-dev-2a59bb',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
