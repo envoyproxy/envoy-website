c2abb75819ceb1cb268ced89860ec00e3e81c512
envoy-docs(travis) <envoy-docs@users.noreply.github.com>
2019-07-19 20:30:20 +0000

    docs envoy@6e6a7ccfeb57f79569feff2d7da128361fcacc33

--- docs/envoy/latest/_static/documentation_options.js
+++ docs/envoy/latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.12.0-dev-6feb15',
+    VERSION: '1.12.0-dev-6e6a7c',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
