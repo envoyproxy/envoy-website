5ffc770a0c6668ac894d00dc8fb26cff0b1ef04f
envoy-docs(Azure Pipelines) <envoy-docs@users.noreply.github.com>
2021-01-31 20:44:43 +0000

    docs envoy@8b1262cfb5e473614ff9d2420130855c9597e1bb

--- docs/envoy/latest/_static/documentation_options.js
+++ docs/envoy/latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.18.0-dev-6fcbe4',
+    VERSION: '1.18.0-dev-8b1262',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
