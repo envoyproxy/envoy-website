d2d4ad723fd697ffae3e1d429dc5bcd5a1fd3ee5
phlax <phlax@users.noreply.github.com>
2022-04-19 22:26:26 +0100

    repo: Add/update docker/compose recipe (#238)
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- site/live.py
+++ site/live.py
@@ -4,7 +4,7 @@ import subprocess
 
 
 def main():
-    subprocess.run(["external/jekyll_bundle/bin/jekyll", "serve", "--watch", "--incremental", "-p", "site/jekyll_plugins"])
+    subprocess.run(["external/jekyll_bundle/bin/jekyll", "serve", "-H", "0.0.0.0", "--watch", "--incremental", "-p", "site/jekyll_plugins"])
 
 
 if __name__ == "__main__":
