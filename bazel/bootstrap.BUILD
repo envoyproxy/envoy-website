c18e21f2f47e72224a896056103313cae6faecaa
Ryan Northey <ryan@synca.io>
2022-04-21 15:30:10 +0100

    bazel: Use archive for bootstrap
    Signed-off-by: Ryan Northey <ryan@synca.io>

---
+++ bazel/bootstrap.BUILD
@@ -0,0 +1,5 @@
+
+exports_files([
+    "dist/js/bootstrap.bundle.min.js",
+    "scss",
+])
--- site/live_docs.py
+++ site/live_docs.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import shutil
 import subprocess
 import tarfile
@@ -7,8 +8,13 @@ import tarfile
 
 def main():
     try:
-        tarfile.open("_sass/vendor/bootstrap.tar.gz").extractall()
         tarfile.open("docs/latest.tar.gz").extractall()
+        if os.path.exists("_sass/vendor/bootstrap"):
+            shutil.rmtree("_sass/vendor/bootstrap")
+        shutil.copytree("external/com_github_twbs_bootstrap/scss", "_sass/vendor/bootstrap")
+        if not os.path.exists("assets/js"):
+            os.makedirs("assets/js")
+        shutil.copy("external/com_github_twbs_bootstrap/dist/js/bootstrap.bundle.min.js", "assets/js/bootstrap.bundle.min.js")
         subprocess.run(["external/jekyll_bundle/bin/jekyll", "serve", "-H", "0.0.0.0", "--watch", "--incremental", "-p", "site/jekyll_plugins"])
     finally:
         shutil.rmtree("docs/envoy/latest")
