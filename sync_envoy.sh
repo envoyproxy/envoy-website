41e79d1fdafbded4c2e204859f35c3b693d9b59b
Ryan Northey <ryan@synca.io>
2023-09-29 19:12:48 +0100

    repo: Sync archive changes
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- sync_envoy.sh
+++ sync_envoy.sh
@@ -4,11 +4,10 @@ set -o pipefail
 
 
 ENVOY_SRC_DIR="${ENVOY_SRC_DIR:-../envoy}"
+ENVOY_ARCHIVE_DIR="${ENVOY_ARCHIVE_DIR:-../archive}"
 ENVOY_VERSION=$(git -C "$ENVOY_SRC_DIR" rev-parse HEAD)
-
-echo "Syncing Envoy -> ${ENVOY_VERSION}"
-
-bazel run //bazel:update envoy "${ENVOY_VERSION}"
+ENVOY_ARCHIVE_VERSION=$(git -C "$ENVOY_ARCHIVE_DIR" rev-parse HEAD)
+UPDATED=
 
 if [[ -n "$COMMITTER_NAME" ]]; then
     git config --global user.name "$COMMITTER_NAME"
@@ -18,8 +17,35 @@ if [[ -n "$COMMITTER_EMAIL" ]]; then
     git config --global user.email "$COMMITTER_EMAIL"
 fi
 
-git commit versions.bzl -m "Sync Envoy @${ENVOY_VERSION}"
+sync_envoy () {
+    echo "Syncing Envoy -> ${ENVOY_VERSION}"
+    bazel run //bazel:update envoy "${ENVOY_VERSION}"
+    if git diff --quiet --exit-code; then
+        echo "No Envoy changes"
+    else
+        git commit versions.bzl -m "Sync Envoy @${ENVOY_VERSION}"
+        git show
+        UPDATED=1
+    fi
+}
 
-git show
+sync_archive () {
+    echo "Syncing Archive -> ${ENVOY_ARCHIVE_VERSION}"
+    bazel run //bazel:update envoy_archive "${ENVOY_ARCHIVE_VERSION}"
+    if git diff --quiet --exit-code; then
+        echo "No Archive changes"
+    else
+        git commit versions.bzl -m "Sync Archive @${ENVOY_ARCHIVE_VERSION}"
+        git show
+        UPDATED=1
+    fi
+}
 
-git push origin HEAD:main
+sync_envoy
+sync_archive
+
+if [[ -n "$UPDATED" ]]; then
+    git push origin HEAD:main
+else
+    echo "Nothing to push"
+fi
