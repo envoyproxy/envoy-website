df612e331a51fcd47837802de298890d095f2c09
Ryan Northey <ryan@synca.io>
2023-10-02 19:09:04 +0100

    build/website: Use envoy unpack tool
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- build-website.sh
+++ build-website.sh
@@ -4,6 +4,7 @@ set -o pipefail
 
 BAZEL="${BAZEL:-bazel}"
 OUTPUT_DIR="${1:-_site}"
+OUTPUT_DIR="$(realpath "${OUTPUT_DIR}")"
 
 if  [[ -e "$OUTPUT_DIR" ]]; then
     echo "Path to build the website (${OUTPUT_DIR}) exists, removing contents"
@@ -11,22 +12,17 @@ if  [[ -e "$OUTPUT_DIR" ]]; then
 fi
 
 mkdir -p "${OUTPUT_DIR}"
-
 BAZEL_BUILD_ARGS=()
 
 if [[ -n "$CI" ]]; then
     BAZEL_BUILD_ARGS=(--config=ci)
 fi
 
-$BAZEL build "${BAZEL_BUILD_ARGS[@]}" //site
-
-echo "Extracting website -> ${OUTPUT_DIR}"
-
 $BAZEL run \
-    @envoy//tools/zstd -- \
-        --stdout \
-        -d "${PWD}/bazel-bin/site/site_html.tar.zst" \
-    | tar -x -C "${OUTPUT_DIR}"
+         "${BAZEL_BUILD_ARGS[@]}" \
+         --@envoy//tools/tarball:target=//site \
+         @envoy//tools/tarball:unpack \
+         "$OUTPUT_DIR"
 
 if [[ -n "$CI" ]]; then
     $BAZEL shutdown || :
