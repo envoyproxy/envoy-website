c5463d17c47b7ab0c794792c8f2ee1691edb0bf2
Ryan Northey <ryan@synca.io>
2023-10-03 08:50:56 +0100

    ci/sync: Separate update workflows and fix
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- sync_archive.sh
+++ sync_archive.sh
@@ -3,8 +3,8 @@
 set -o pipefail
 
 
-ENVOY_SRC_DIR="${ENVOY_SRC_DIR:-../envoy}"
-ENVOY_VERSION=$(git -C "$ENVOY_SRC_DIR" rev-parse HEAD)
+ENVOY_ARCHIVE_DIR="${ENVOY_ARCHIVE_DIR:-../archive}"
+ENVOY_ARCHIVE_VERSION=$(git -C "$ENVOY_ARCHIVE_DIR" rev-parse HEAD)
 UPDATED=
 
 if [[ -n "$COMMITTER_NAME" ]]; then
@@ -15,19 +15,19 @@ if [[ -n "$COMMITTER_EMAIL" ]]; then
     git config --global user.email "$COMMITTER_EMAIL"
 fi
 
-sync_envoy () {
-    echo "Syncing Envoy -> ${ENVOY_VERSION}"
-    bazel run //bazel:update envoy "${ENVOY_VERSION}"
+sync_archive () {
+    echo "Syncing Archive -> ${ENVOY_ARCHIVE_VERSION}"
+    bazel run //bazel:update envoy_archive "${ENVOY_ARCHIVE_VERSION}"
     if git diff --quiet --exit-code; then
-        echo "No Envoy changes"
+        echo "No Archive changes"
     else
-        git commit versions.bzl -m "Sync Envoy @${ENVOY_VERSION}"
+        git commit versions.bzl -m "Sync Archive @${ENVOY_ARCHIVE_VERSION}"
         git show
         UPDATED=1
     fi
 }
 
-sync_envoy
+sync_archive
 
 if [[ -n "$UPDATED" ]]; then
     git push origin HEAD:main
