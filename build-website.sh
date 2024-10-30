c088428459d7941769989ce869c95b4d1c2463a7
Ryan Northey <ryan@synca.io>
2024-10-30 14:22:37 +0000

    bazel: Add debugging on fail
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- build-website.sh
+++ build-website.sh
@@ -5,6 +5,21 @@ set -o pipefail
 BAZEL="${BAZEL:-bazel}"
 OUTPUT_DIR="${1:-_site}"
 OUTPUT_DIR="$(realpath "${OUTPUT_DIR}")"
+OUTPUT_BASE="$($BAZEL info output_base 2>/dev/null)"
+
+
+debug_jvm_fail () {
+    RETURN_CODE="$?"
+    JVM_OUT="$(find "${OUTPUT_BASE}" -name "jvm.out" 2>/dev/null)"
+
+    if [[ -n "$JVM_OUT" && -s "$JVM_OUT" ]]; then
+        echo
+        echo "JVM out: ${JVM_OUT}"
+        cat "$JVM_OUT"
+        echo
+    fi
+    return "$RETURN_CODE"
+}
 
 if  [[ -e "$OUTPUT_DIR" ]]; then
     echo "Path to build the website (${OUTPUT_DIR}) exists, removing contents"
@@ -18,11 +33,12 @@ if [[ -n "$CI" ]]; then
     BAZEL_BUILD_ARGS=(--config=ci)
 fi
 
+
 $BAZEL run \
          "${BAZEL_BUILD_ARGS[@]}" \
          --@envoy//tools/tarball:target=//site \
          @envoy//tools/tarball:unpack \
-         "$OUTPUT_DIR"
+         "$OUTPUT_DIR" || debug_jvm_fail
 
 if [[ -n "$CI" ]]; then
     $BAZEL --timeout=5 shutdown > /dev/null 2>&1 || :
