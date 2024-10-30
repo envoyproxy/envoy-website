16fea1736710755c74ec824b3696b23cf21800a2
Ryan Northey <ryan@synca.io>
2024-10-30 14:22:37 +0000

    bazel: Fix server death flake
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- build-website.sh
+++ build-website.sh
@@ -10,16 +10,32 @@ OUTPUT_BASE="$($BAZEL info output_base 2>/dev/null)"
 
 debug_jvm_fail () {
     RETURN_CODE="$?"
+    echo "Bazel exited with error: ${RETURN_CODE}"
     JVM_OUT="$(find "${OUTPUT_BASE}" -name "jvm.out" 2>/dev/null)"
 
     if [[ -n "$JVM_OUT" && -s "$JVM_OUT" ]]; then
         echo
-        echo "JVM out: ${JVM_OUT}"
+        echo "jvm.out: ${JVM_OUT}"
         cat "$JVM_OUT"
         echo
     fi
     return "$RETURN_CODE"
 }
+
+inject_ci_bazelrc () {
+    {
+
+        PROC_COUNT="$(nproc)"
+        PROCS=$((PROC_COUNT - 1))
+        SPHINX_ARGS="-j 12 -v warn"
+        echo "build:ci --action_env=SPHINX_RUNNER_ARGS=\"${SPHINX_ARGS}\""
+        # echo "build:ci --local_ram_resources=20480"
+
+    } > repo.bazelrc
+
+    cat repo.bazelrc
+}
+
 
 if  [[ -e "$OUTPUT_DIR" ]]; then
     echo "Path to build the website (${OUTPUT_DIR}) exists, removing contents"
@@ -31,8 +47,8 @@ BAZEL_BUILD_ARGS=()
 
 if [[ -n "$CI" ]]; then
     BAZEL_BUILD_ARGS=(--config=ci)
+    inject_ci_bazelrc
 fi
-
 
 $BAZEL run \
          "${BAZEL_BUILD_ARGS[@]}" \
