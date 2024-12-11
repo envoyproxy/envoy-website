a31db786fca52a54be6ad239cbd2c6a24340c45e
Ryan Northey <ryan@synca.io>
2024-12-11 15:13:27 +0000

    ci/netlify: Add path ignore
    Signed-off-by: Ryan Northey <ryan@synca.io>

---
+++ netlify-should-run.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash -e
+
+set -o pipefail
+
+IGNORED_PATHS=(
+    docker-compose.yml
+    docker
+    netlify-should-run.sh)
+TARGET_BRANCH=main
+readarray -t CHANGED_FILES < <(git diff --name-only "$TARGET_BRANCH")
+
+for FILE in "${CHANGED_FILES[@]}"; do
+    if ! [[ " ${WATCH_PATHS[@]} " =~ " ${FILE%%/*} " ]] && ! [[ " ${WATCH_PATHS[@]} " =~ " ${FILE} " ]]; then
+        exit 1
+    fi
+done
+
+exit 0
